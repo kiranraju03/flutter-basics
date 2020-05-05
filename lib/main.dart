748567b94c04349881f8a6c9d42cc62fd6c9a19f
kiranraju03 <kiran.raju.k.43@gmail.com>
2020-05-05 21:38:49 +0530

    Converting the stateless to stateful widget and state management example

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,34 @@ void main() {
   runApp(MyApp());
 }
 
-//StatelessWidget is a base class for creating own widgets
-class MyApp extends StatelessWidget {
+//MyApp responsible for re-rendering the widget component on change
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    //TODO: implement createState
+    return MyAppState();
+  }
+}
+
+//State is a generic element, so specifying the type of state to be MyApp in the angular braces
+//Below class is where the state of the element gets updated, and gets re-rendered using the statefulwidget
+//So State generic has to be connected to the stateful widget, by overriding createstate
+class MyAppState extends State<MyApp> {
+  var arrayIndexValue = 0;
+
+  void clickedAction() {
+    //Setting the state of the arrayIndex, on which the re-render cycle willl be triggered
+    setState(() {
+      arrayIndexValue = arrayIndexValue + 1;
+    });
+    print('ClickAction Called');
+  }
+
   //build method is called by flutter, which hold the metadata for building the app
   Widget build(BuildContext context) {
+    var arrayValues = ['Kiran Raju', 'Kiran Raju Krishna'];
+    //Function call done from clicking 1st button
+
     //material app provides a app with some of the inbuilt widgets using named arguments
     //named arguments are used, as material app has numerous parameters that it accepts
     //here we are accessing only the home parameter to return on the app page
@@ -24,15 +48,16 @@ class MyApp extends StatelessWidget {
           //Column expects a list of widgets, which act as its children
           body: Column(
             children: [
-              Text('Hello! Kiran'),
+              Text(arrayValues[arrayIndexValue]),
               //A button which is disabled
               RaisedButton(
                 child: Text('Button1'),
-                onPressed: null,
+                onPressed: clickedAction,
               ),
               RaisedButton(
                 child: Text('Button2'),
-                onPressed: null,
+                //Creating anonymous function
+                onPressed: () => print('Second button clicked'),
               )
             ],
           )),
